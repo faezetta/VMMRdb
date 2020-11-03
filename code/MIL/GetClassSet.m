ecf47820b851d84d71b99347d412301aa447f07d
Faezeh Tafazzoli <faezetta@users.noreply.github.com>
2020-11-03 10:38:05 -0800

    Delete GetClassSet.m

--- code/MIL/GetClassSet.m
+++
@@ -1,8 +0,0 @@
-function [class_set, num_class] = GetClassSet(Y)
-
-global preprocess;
-class_set = unique(Y);
-if (isfield(preprocess, 'ClassSet')),
-    class_set = preprocess.ClassSet;
-end;
-num_class = length(class_set);
